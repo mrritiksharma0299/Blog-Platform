84af70183778836dc38d79608c0893bc0dfd8cf6
mrritiksharma0299 <mrritiksharma0299@gmail.com>
2026-07-13 13:33:29 +0530

    Configure Cloudinary SDK

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 import os
 
+import cloudinary
+
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = PROJECT_DIR.parent
 
@@ -212,3 +214,10 @@ CLOUDINARY_STORAGE = {
     "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
     "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
+
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+)
